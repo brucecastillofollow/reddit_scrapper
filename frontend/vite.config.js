8fdd4fc6f1bd25b2cf5745bf1e19dbbd41382b91
brucecastillofollow <fanbackend2@gmail.com>
2026-05-19 21:40:40 +0000

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://reddit.weienwong.online/api',
         changeOrigin: true,
       },
     },
